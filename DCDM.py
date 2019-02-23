535a1e8aed6210084e7556530a84d9f87209c950
LiquidatorCoder <42910433+LiquidatorCoder@users.noreply.github.com>
2019-02-23 16:19:02 +0530

    DCDM (Added SSL Certificate Verification)

--- DCDM.py
+++ DCDM.py
@@ -5,6 +5,15 @@ import re
 import os
 import keyboard
 import time
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_equiv( True ) 
